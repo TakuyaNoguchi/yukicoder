8193a0774fe3d06f308f862887cd094ed28a5d95
Takuya Noguchi <takuya@netlab.jp>
2018-01-28 15:40:22 +0900

    No.481 1から10 (再提出)

--- solved/400_499/481/answer.rb
+++ solved/400_499/481/answer.rb
@@ -1,17 +1,3 @@
 b = gets.split.map(&:to_i)
 
-if b.first != 1
-  puts 1
-  exit
-end
-
-if b.last != 10
-  puts 10
-  exit
-end
-
-puts b.reject.with_index { |n, i|
-  next true if i.zero?
-
-  (n - b[i - 1]) == 1
-}.first - 1
+puts (1..10).inject(&:+) - b.inject(&:+)
