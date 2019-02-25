664d1956f1019ca812ff1e61a520fec409dbbaea
Takuya Noguchi <takuya@netlab.jp>
2019-02-26 00:03:25 +0900

    No.544 Delete 7
    * リファクタリング

--- solved/500_599/544/answer.rb
+++ solved/500_599/544/answer.rb
@@ -1,13 +1,12 @@
-N = gets.to_i
 a, b = [], []
 
-N.to_s.each_char do |n|
-  if n == '7'
-    a << 6
-    b << 1
+gets.chomp.each_char do |num_char|
+  if num_char == '7'
+    a << '6'
+    b << '1'
   else
-    a << n
-    b << 0
+    a << num_char
+    b << '0'
   end
 end
 
